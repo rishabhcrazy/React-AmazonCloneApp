6c4699e34de80e0cb4240dc6bfeb73235fc087a9
rishabhk81 <rishabhkr.rishu@live.com>
2021-03-13 04:18:18 +0530

    Amazon Nav Bar

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Header from './Header.js';
+function App() {
+  return (
+    //BEM convention?
+    <div className="App">
+      <Header/>
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZTfO2iFCKDGyGZwjbkDFp9HaAiLNkavM",
+    authDomain: "challenge-2bcfa.firebaseapp.com",
+    projectId: "challenge-2bcfa",
+    storageBucket: "challenge-2bcfa.appspot.com",
+    messagingSenderId: "671685694585",
+    appId: "1:671685694585:web:d07e02fdaaed98989530a8",
+    measurementId: "G-72N8NNVHBS"
+  };
