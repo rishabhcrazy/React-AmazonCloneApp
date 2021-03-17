1f4b9e85805831d1a9caa8420619d799c7c31ba7
rishabhk81 <rishabhkr.rishu@live.com>
2021-03-17 05:35:51 +0530

    Home Page with products added.

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
+import Product from './Product.js'
 function Home() {
     return (
         <div className = 'home'>
@@ -12,17 +13,57 @@ function Home() {
             </div>
 
             <div className = 'home_row'>
-                {/* Product */}
+                <Product
+                    id = '100000'
+                    title='The Lean startup'
+                    price = {299}
+                    image = 'https://m.media-amazon.com/images/I/81vvgZqCskL._AC_UY218_.jpg'
+                    rating = {5}
+                 />
+                <Product
+                    id = '100001'
+                    title='All-new Echo Dot (4th Gen)'
+                    price = {3999}
+                    image = 'https://m.media-amazon.com/images/I/41dayzt6quL._AC_UY218_.jpg'
+                    rating = {5}
+                />
             </div>
 
             <div className = 'home_row'>
-                {/* Product */}
+                <Product
+                        id = '100011'
+                        title='All-new Echo Dot (4th Gen) with clock'
+                        price = {4999}
+                        image = 'https://m.media-amazon.com/images/I/41wssh5xNIL._AC_UY218_.jpg'
+                        rating = {5}
+                    />
+                <Product
+                    id = '100012'
+                    title='Echo Dot (3rd Gen) – Smart speaker with Alexa (Purple)'
+                    price = {3499}
+                    image = 'https://m.media-amazon.com/images/I/61V25P7mlyL._AC_UY218_.jpg'
+                    rating = {5}
+                />
+                <Product
+                    id = '100013'
+                    title='Echo Dot (3rd Gen, Grey) Combo with Wipro 9W LED Smart Color Bulb'
+                    price = {3599}
+                    image = 'https://m.media-amazon.com/images/I/61mmpVq+D4L._AC_UY218_.jpg'
+                    rating = {5}
+                />
+                
             </div>
 
             <div className = 'home_row'>
-                {/* Product */}
+                 <Product
+                    id = '100020'
+                    title='ntroducing Echo Show 8 – Smart display with Alexa'
+                    price = {8999}
+                    image = 'https://m.media-amazon.com/images/I/51UZ17lvVFL._AC_UY218_.jpg'
+                    rating = {5}
+                />
             </div>
-            
+
         </div>
     )
 }
